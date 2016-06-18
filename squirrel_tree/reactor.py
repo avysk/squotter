0d28d2f592f2809ac0cc4f1aec8367694246ab84
Alexey Vyskubov <alexey@hotmail.fi>
2016-06-18 19:28:49 +0300

    Reformatting

--- squirrel_tree/reactor.py
+++ squirrel_tree/reactor.py
@@ -1,19 +1,18 @@
-"""
-Reactor: the object containing callbacks for trie.
-"""
+"""Reactor: the object containing callbacks for trie."""
 from abc import ABCMeta, abstractmethod
 
 
 class Reactor(object):
-    """
-    Reactor interface.
-    """
+
+    """Reactor interface."""
+
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def delete_callback(self, parent, suffix):
         """
         Called when a terminal key is completely removed.
+
         :param parent: The parent of suffix to be removed
         :param key:  The suffix which is removed.
         :return: nothing
@@ -22,16 +21,17 @@ class Reactor(object):
     @abstractmethod
     def insert_callback(self, chain, value):
         """
-        Called when a value is inserted into a Trie
+        Called when a value is inserted into a Trie.
+
         :param chain: list of relative keys under which value is inserted
         :param value: the value being inserted
-        :return:
         """
 
     @abstractmethod
     def create_callback(self, chain):
         """
-        Called when a subtrie for new key is created
+        Called when a subtrie for new key is created.
+
         :param chain: the chain of parent keys, ending with the key of created
         Trie
         """
@@ -39,19 +39,19 @@ class Reactor(object):
     @abstractmethod
     def move_callback(self, old_chain, old_key, new_chain, new_key):
         """
-        Called when a subtree is moved
+        Called when a subtree is moved.
+
         :param old_chain: the old location of the subtree
         :param old_key: the key of subtree under old location
         :param new_chain: the new location of the subtree
         :param key: the key of subtree under new location
-        :return:
         """
 
 
 class EmptyReactor(Reactor):
-    """
-    Reactor which does nothing.
-    """
+
+    """Reactor which does nothing."""
+
     def delete_callback(self, parent, suffix):
         pass
 
